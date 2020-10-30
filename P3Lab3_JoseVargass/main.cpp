6602f46586e4d06fdc8eaed93fa269abfaf91114
Jose Vargas <jose.vargasr@unitec.edu>
2020-10-30 12:53:18 -0600

    Subir archivo

---
+++ P3Lab3_JoseVargass/main.cpp
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: josevargas
+ *
+ * Created on October 30, 2020, 12:50 PM
+ */
+
+#include <iostream>
+
+using std::cout;
+using std::endl;
+using std::cin;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    int tam = 0;
+    cout << "Ingrese el tamaño de la matriz: ";
+    cin >> tam;
+    
+    return 0;
+}
+
